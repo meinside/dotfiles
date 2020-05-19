36bce248f3f0a5db44183307ec3ed216bb1eaef9
Sungjin Han <meinside@gmail.com>
2020-05-19 09:51:33 +0900

    Edit bin/install_go.sh (go 1.14.3)

--- bin/install_go.sh
+++ bin/install_go.sh
@@ -9,7 +9,7 @@
 # - or create a (temporary) swap partition.
 # 
 # created on : 2014.07.01.
-# last update: 2020.04.09.
+# last update: 2020.05.19.
 # 
 # by meinside@gmail.com
 
@@ -31,7 +31,7 @@ BOOTSTRAP_BRANCH="release-branch.go1.4"
 
 # XXX - edit for different version of Go (see: https://go.googlesource.com/go/+refs)
 #INSTALL_BRANCH="release-branch.go1.6"	# branch
-INSTALL_BRANCH="go1.14.2"	# tag
+INSTALL_BRANCH="go1.14.3"	# tag
 
 function prep {
 	# install essential packages
