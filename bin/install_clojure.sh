1a069a9e1086da76fda14a239dc8be61d1f4a13e
Sungjin Han <meinside@gmail.com>
2020-10-26 10:36:02 +0900

    Edit bin/install_clojure.sh

--- bin/install_clojure.sh
+++ bin/install_clojure.sh
@@ -6,7 +6,7 @@
 #
 # (https://clojure.org/guides/getting_started#_installation_on_linux)
 # 
-# last update: 2020.10.23.
+# last update: 2020.10.26.
 # 
 # by meinside@gmail.com
 
@@ -36,7 +36,7 @@ JDK_DIR=/opt/jdk
 # ARM64   : https://www.azul.com/downloads/zulu-community/?architecture=arm-64-bit&package=jdk
 PLATFORM=`uname -m`
 case "$PLATFORM" in
-	arm64|aarch64) ZULU_EMBEDDED_VERSION="zulu11.41.75-ca-jdk11.0.8-linux_aarch64" ;;
+	arm64|aarch64) ZULU_EMBEDDED_VERSION="zulu11.43.88-ca-jdk11.0.9-linux_aarch64" ;;
 	armv7*) ZULU_EMBEDDED_VERSION="zulu11.41.75-ca-jdk11.0.8-linux_aarch32hf" ;;
 	x86*) ZULU_EMBEDDED_VERSION="" ;;
 	*) echo "* Unsupported platform: $PLATFORM"; exit 1 ;;
