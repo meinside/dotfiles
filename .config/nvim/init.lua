be9a4c494362d3c4862bf3ea82a740f849ab3dfd
Sungjin Han <meinside@gmail.com>
2021-05-28 18:04:56 +0900

    Fixing .config/nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -152,72 +152,6 @@ require('packer').startup(function()
 
 end)
 
-------------------------
--- treesitter for syntax highlighting
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {'go', 'python', 'ruby', 'rust', 'zig'};
-  highlight = {enable = true};
-}
-
-------------------------
--- compe for autocompletion
-require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = true;
-
-  source = {
-    path = true;
-    nvim_lsp = true;
-  };
-}
-
-local t = function(str)
-  return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-local check_back_space = function()
-  local col = fn.col('.') - 1
-  if col == 0 or fn.getline('.'):sub(col, col):match('%s') then
-    return true
-  else
-    return false
-  end
-end
-
--- Use (s-)tab to:
---- move to prev/next item in completion menuone
---- jump to prev/next snippet's placeholder
-_G.tab_complete = function()
-  if fn.pumvisible() == 1 then
-    return t "<C-n>"
-  elseif check_back_space() then
-    return t "<Tab>"
-  else
-    return fn['compe#complete']()
-  end
-end
-_G.s_tab_complete = function()
-  if fn.pumvisible() == 1 then
-    return t "<C-p>"
-  else
-    return t "<S-Tab>"
-  end
-end
-
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
 ------------------------
 -- for language server configuration
@@ -292,13 +226,82 @@ require'lspconfig'.zls.setup{ -- zig
 g['rustfmt_autosave'] = 1 -- :RustFmt
 
 ------------------------
--- gitgutter
-require('gitsigns').setup{}
+-- treesitter for syntax highlighting
+require'nvim-treesitter.configs'.setup {
+  ensure_installed = {'go', 'python', 'ruby', 'rust', 'zig'};
+  highlight = {enable = true};
+}
 
+------------------------
+-- compe for autocompletion
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = true;
+
+  source = {
+    path = true;
+    buffer = true;
+    calc = true;
+    nvim_lsp = true;
+    nvim_lua = true;
+  };
+}
+
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+  local col = fn.col('.') - 1
+  if col == 0 or fn.getline('.'):sub(col, col):match('%s') then
+    return true
+  else
+    return false
+  end
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menuone
+--- jump to prev/next snippet's placeholder
+_G.tab_complete = function()
+  if fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return fn['compe#complete']()
+  end
+end
+_G.s_tab_complete = function()
+  if fn.pumvisible() == 1 then
+    return t "<C-p>"
+  else
+    return t "<S-Tab>"
+  end
+end
+
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 
 ------------------------------------------------
 -- linting
 -- TODO
+
+------------------------
+-- gitgutter
+require('gitsigns').setup{}
 
 ------------------------------------------------
 -- other settings
@@ -312,7 +315,6 @@ opt('b', 'sw', 4)
 opt('b', 'sts', 4)
 opt('o', 'fencs', 'ucs-bom,utf-8,korea')
 opt('o', 'showbreak', '↳')
-opt('w', 'breakindent', true)
 
 cmd [[set mouse-=a]]  -- not to enter visual mode when dragging text
 cmd [[set nobackup]]  -- do not keep a backup file, use versions instead
@@ -325,6 +327,8 @@ cmd [[set ai]]
 cmd [[set smartindent]]
 cmd [[syntax on]]
 cmd [[set hlsearch]]
+cmd [[set breakindent]]
+cmd [[set wildmenu]]
 
 map('n', '<C-h>', ':tabprevious<CR>') -- <ctrl-h> for previous tab,
 map('n', '<C-l>', ':tabnext<CR>') -- <ctrl-l> for next tab,
