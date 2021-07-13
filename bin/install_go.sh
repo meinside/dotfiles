7079625e35b14643082ca3bbe8e1880a22dc5a60
Sungjin Han <meinside@gmail.com>
2021-07-13 21:35:20 +0900

    Edit bin/install_go.sh (go version 1.16.6)

--- bin/install_go.sh
+++ bin/install_go.sh
@@ -11,7 +11,7 @@
 # *** Another Note: `aarch64` is not supported in golang 1.4, so it will be bootstrapped with package manager's version.
 # 
 # created on : 2014.07.01.
-# last update: 2021.06.10.
+# last update: 2021.07.13.
 # 
 # by meinside@gmail.com
 
@@ -33,7 +33,7 @@ BOOTSTRAP_BRANCH="release-branch.go1.4"
 
 # XXX - edit for different version of Go (see: https://go.googlesource.com/go/+refs)
 #INSTALL_BRANCH="release-branch.go1.6"	# branch
-INSTALL_BRANCH="go1.16.5"	# tag
+INSTALL_BRANCH="go1.16.6"	# tag
 
 function prep {
 	# install essential packages
