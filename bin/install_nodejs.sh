b15b143b264118f0d39e15205785e2f3d55e0cf0
Sungjin Han <meinside@gmail.com>
2021-10-27 11:24:07 +0900

    Edit bin/install_nodejs.sh (version 16.13.0)

--- bin/install_nodejs.sh
+++ bin/install_nodejs.sh
@@ -5,7 +5,7 @@
 # install pre-built Node.js (LTS) for Linux from: https://nodejs.org/dist
 # 
 # created on : 2013.07.19.
-# last update: 2021.10.13.
+# last update: 2021.10.27.
 # 
 # by meinside@gmail.com
 
@@ -18,7 +18,7 @@ GREEN="\033[0;32m"
 YELLOW="\033[0;33m"
 RESET="\033[0m"
 
-VERSION="14.18.1"	# XXX - edit this for other versions
+VERSION="16.13.0"	# XXX - edit this for other versions
 PLATFORM=`uname -m`	# armv7l, armv6l, ...
 
 # x86_64 = x64, aarch64 = arm64
