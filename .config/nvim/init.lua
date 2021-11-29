8da0b2f000d7edfbced05b8c24f432cf8c34377f
Sungjin Han <meinside@gmail.com>
2021-11-29 10:00:26 +0900

    Edit .config/nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,7 @@
 -- created by meinside@gmail.com,
 --
 -- created on : 2021.05.27.
--- last update: 2021.11.24.
+-- last update: 2021.11.27.
 
 ------------------------------------------------
 -- helpers
@@ -433,6 +433,27 @@ require('packer').startup(function()
   -- diagnostics
   vim.api.nvim_exec([[
     autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()
+  ]], false)
+
+
+  -- lspkind colors
+  vim.api.nvim_exec([[
+    " gray
+    highlight! CmpItemAbbrDeprecated guibg=NONE gui=strikethrough guifg=#808080
+    " blue
+    highlight! CmpItemAbbrMatch guibg=NONE guifg=#569CD6
+    highlight! CmpItemAbbrMatchFuzzy guibg=NONE guifg=#569CD6
+    " light blue
+    highlight! CmpItemKindVariable guibg=NONE guifg=#9CDCFE
+    highlight! CmpItemKindInterface guibg=NONE guifg=#9CDCFE
+    highlight! CmpItemKindText guibg=NONE guifg=#9CDCFE
+    " pink
+    highlight! CmpItemKindFunction guibg=NONE guifg=#C586C0
+    highlight! CmpItemKindMethod guibg=NONE guifg=#C586C0
+    " front
+    highlight! CmpItemKindKeyword guibg=NONE guifg=#D4D4D4
+    highlight! CmpItemKindProperty guibg=NONE guifg=#D4D4D4
+    highlight! CmpItemKindUnit guibg=NONE guifg=#D4D4D4
   ]], false)
 
 
