a174c18110667917dfbd559a101258a758ecb2f5
Sungjin Han <meinside@gmail.com>
2021-12-03 15:26:45 +0900

    Edit .config/nvim/init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,7 +3,7 @@
 -- created by meinside@gmail.com,
 --
 -- created on : 2021.05.27.
--- last update: 2021.12.02.
+-- last update: 2021.12.03.
 
 ------------------------------------------------
 -- helpers
@@ -62,7 +62,7 @@ local on_attach = function(client, bufnr)
   fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
 
   -- LSP capabilities
-  local capabilities = vim.lsp.protocol.make_client_capabilities()
+  local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
   capabilities.textDocument.completion.completionItem.documentationFormat = { 'markdown', 'plaintext' }
   capabilities.textDocument.completion.completionItem.snippetSupport = true
   capabilities.textDocument.completion.completionItem.preselectSupport = true
