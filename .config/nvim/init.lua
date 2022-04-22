9a57fe3f2694aa3716ad92fbc7c241be687d6a9f
Sungjin Han <meinside@gmail.com>
2022-04-22 21:33:01 +0900

    Fix not to load lua-language-server when it doesn't exist

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -507,17 +507,19 @@ require('packer').startup({function()
   end
   -------- other language servers for custom setup --------
   -- (lua)
-  local runtime_path = vim.split(package.path, ';')
-  table.insert(runtime_path, "lua/?.lua")
-  table.insert(runtime_path, "lua/?/init.lua")
-  nvim_lsp['sumneko_lua'].setup {
-    settings = { Lua = {
-      runtime = { version = 'LuaJIT', path = runtime_path },
-      diagnostics = { globals = {'vim'} },
-      workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-      telemetry = { enable = false },
-    } },
-  }
+  if fn.empty(fn.glob('/opt/lua-language-server')) > 0 then
+    local runtime_path = vim.split(package.path, ';')
+    table.insert(runtime_path, "lua/?.lua")
+    table.insert(runtime_path, "lua/?/init.lua")
+    nvim_lsp['sumneko_lua'].setup {
+      settings = { Lua = {
+        runtime = { version = 'LuaJIT', path = runtime_path },
+        diagnostics = { globals = {'vim'} },
+        workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+        telemetry = { enable = false },
+      } },
+    }
+  end
 
 end, config = {autoremove = true, display = {open_fn = require'packer.util'.float}}})
 
