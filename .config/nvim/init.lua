8ec4b4b26b7af5c97f7b3ac3698889edf8031772
Sungjin Han <meinside@duck.com>
2023-03-22 11:01:59 +0900

    Edit .config/nvim things

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,29 +3,35 @@
 -- created by meinside@duck.com,
 --
 -- created on : 2021.05.27.
--- last update: 2023.01.31.
+-- last update: 2023.03.22.
 
 
 ------------------------------------------------
 -- my helper functions
--- NOTE: in file: `.config/nvim/lua/tools.lua`
+-- NOTE: in file: `~/.config/nvim/lua/tools.lua`
 _G['tools'] = require'tools'
 
 
 ------------------------------------------------
 -- my custom things
--- NOTE: in file: `.config/nvim/lua/locals/init.lua`
+-- NOTE: in file: `~/.config/nvim/lua/locals/init.lua`
 _G['locals'] = require'locals'
 
 
 ------------------------------------------------
 -- load neovim plugins
--- NOTE: in file: `.config/nvim/lua/plugins.lua`
+-- NOTE: in file: `~/.config/nvim/lua/plugins.lua`
 require'plugins'
 
 
 ------------------------------------------------
 -- other common settings
--- NOTE: in file: `.config/nvim/lua/settings.lua`
+-- NOTE: in file: `~/.config/nvim/lua/settings.lua`
 require'settings'
+
+
+------------------------------------------------
+-- for developing and testing my plugins
+-- NOTE: in file: `~/.config/nvim/lua/XXXX.lua`
+--_G['XXXX'] = require'XXXX'
 
