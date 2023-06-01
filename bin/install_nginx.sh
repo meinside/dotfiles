e104c03ab05928cbce6d68c7ac779eb6bcc90b92
Sungjin Han <meinside@duck.com>
2023-06-01 17:48:49 +0900

    Edit bin/install_nginx.sh

--- bin/install_nginx.sh
+++ bin/install_nginx.sh
@@ -41,7 +41,7 @@
 #   0 5 1 */1 * sudo tailscale cert --cert-file /path/to/cert.crt --key-file /path/to/cert.key "subdomain.my-tailnet-name.ts.net"
 #
 # created on : 2017.08.16.
-# last update: 2023.05.22.
+# last update: 2023.06.01.
 # 
 # by meinside@duck.com
 
@@ -52,7 +52,7 @@
 
 # nginx/library versions
 NGINX_VERSION="1.24.0"  # https://nginx.org/en/download.html
-OPENSSL_VERSION="3.1.0" # https://github.com/openssl/openssl/tags
+OPENSSL_VERSION="3.1.1" # https://github.com/openssl/openssl/tags
 ZLIB_VERSION="1.2.13"	# https://github.com/madler/zlib/tags
 PCRE_VERSION="10.42"	# https://github.com/PCRE2Project/pcre2/releases
 
