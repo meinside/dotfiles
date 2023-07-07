67f4044bb1b7d846bf2d2663f5183c6acbebe07d
Sungjin Han <meinside@duck.com>
2023-07-07 16:37:21 +0900

    Edit bin/steamdeck/unlock.sh to install `inetutils`

--- bin/steamdeck/unlock.sh
+++ bin/steamdeck/unlock.sh
@@ -5,11 +5,11 @@
 # Unlocks steam deck's read-only file system.
 #
 # created on : 2022.09.27.
-# last update: 2023.07.05.
+# last update: 2023.07.07.
 
 sudo steamos-readonly disable && \
 	sudo pacman-key --init && \
 	sudo pacman-key --populate archlinux && \
 	sudo pacman -Syu base-devel && \
-	sudo pacman -S glibc linux-api-headers
+	sudo pacman -S glibc linux-api-headers inetutils
 
