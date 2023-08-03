0b85be0bbdfe0cc6156923c717e7d2af852946cf
Sungjin Han <sun.han@tosslab.com>
2023-08-03 16:24:39 +0900

    Add .config/wezterm/wezterm.lua for wezterm

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+-- $XDG_CONFIG_HOME/wezterm/wezterm.lua
+--
+-- last update: 2023.08.03.
+
+local wezterm = require 'wezterm'
+
+local config = {}
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+--------------------------------
+-- my configurations
+config.color_scheme = 'Peppermint (Gogh)'
+config.font = wezterm.font('JetBrainsMono NF')
+config.initial_cols = 160
+config.initial_rows = 50
+config.window_padding = { left = '0.5cell', right = '0.5cell', top = 0, bottom = 0 }
+--
+--------------------------------
+
+return config
+
