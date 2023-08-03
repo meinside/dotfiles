c7951edae190ca1e9ab1ef6fd0155b276d878e9d
Sungjin Han <meinside@duck.com>
2023-08-03 09:50:01 +0900

    Edit .config/nvim things

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -86,7 +86,6 @@ require'lazy'.setup({
         highlight_overrides = {
           mocha = function(C)
             return {
-              TabLineSel = { bg = C.pink },
               CmpBorder = { fg = C.surface2 },
               Pmenu = { bg = C.none },
               TelescopeBorder = { link = 'FloatBorder' },
