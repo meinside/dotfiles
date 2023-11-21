2f2ee317c60bb54c80a8d223ba58021ffa4dc0ea
Sungjin Han <meinside@meinside.dev>
2023-11-21 09:40:04 +0900

    Edit bin/steamdeck things

--- bin/steamdeck/unlock.sh
+++ bin/steamdeck/unlock.sh
@@ -11,11 +11,13 @@
 #
 #
 # created on : 2022.09.27.
-# last update: 2023.11.20.
+# last update: 2023.11.21.
 
 sudo steamos-readonly disable && \
 	sudo pacman-key --init && \
-	sudo pacman-key --populate archlinux && \
+	sudo pacman-key --populate archlinux holo && \
+	sudo pacman -Sy archlinux-keyring holo-keyring && \
+	sudo pacman-key --refresh-keys && \
 	sudo pacman -Syu base-devel && \
 	sudo pacman -S glibc linux-api-headers inetutils
 
