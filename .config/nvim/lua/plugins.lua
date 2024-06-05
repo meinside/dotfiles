df52f48ca217c0bb133f13b84ab85ea6061b5948
Sungjin Han <meinside@meinside.dev>
2024-06-05 11:37:48 +0900

    Edit .config/nvim/lua/plugins.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -4,7 +4,7 @@
 --
 -- NOTE: this will be sourced from: ~/.config/nvim/init.lua
 --
--- last update: 2024.05.30.
+-- last update: 2024.06.05.
 
 
 ------------------------------------------------
@@ -976,12 +976,19 @@ require'lazy'.setup({
 
       vim.api.nvim_create_autocmd('BufWritePre', {
         pattern = '*.go',
-        callback = function() require'go.format'.goimport() end,
+        callback = function() require'go.format'.goimports() end,
+        group = vim.api.nvim_create_augroup('GoFormat', {}),
       })
     end,
-    ft = { 'go' },
+    event = { 'CmdlineEnter' },
+    ft = { 'go', 'gomod' },
+    build = ':lua require("go.install").update_all_sync()',
+    dependencies = {
+      'ray-x/guihua.lua',
+      'neovim/nvim-lspconfig',
+      'nvim-treesitter/nvim-treesitter',
+    },
   },
-  { 'ray-x/guihua.lua', build = 'cd lua/fzy && make' },
   {
     -- install `delve` with :Mason, :DapContinue for starting debugging
     'leoluz/nvim-dap-go', config = function()
