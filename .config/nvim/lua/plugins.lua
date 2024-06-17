ceb60dfa835afa595fb07b653207fb8ed73a7786
Sungjin Han <meinside@meinside.dev>
2024-06-17 18:18:36 +0900

    Edit .config/nvim things

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1068,7 +1068,7 @@ require'lazy'.setup({
       require'gemini'.setup {
         configFilepath = '~/.config/gemini.nvim/config.json',
         timeout = 30 * 1000,
-        model = 'gemini-1.0-pro',
+        model = 'gemini-1.5-pro-latest',
         safetyThreshold = 'BLOCK_ONLY_HIGH',
         stripOutermostCodeblock = function()
           return vim.bo.filetype ~= 'markdown'
