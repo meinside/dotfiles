781d82147cd75924e767f910590b9c3d9bf14066
Sungjin Han <meinside@meinside.dev>
2024-07-31 11:37:22 +0900

    Edit .config/emacs/init.el

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -1,7 +1,13 @@
 ;; ~/.config/emacs/init.el
 ;;
 ;; created on : 2024.07.30.
-;; last update: 2024.07.30.
+;; last update: 2024.07.31.
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; global configurations
+;;
 
 (set-language-environment "UTF-8")
 
@@ -11,27 +17,45 @@
 
 (global-display-line-numbers-mode t)
 
+(setq-default tab-width 4)
+
+;; custom file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;; tree-sitter
+(require 'treesit)
+
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-;; packages
+;; configurations for packages
 ;;
-(require 'package)
 
+(require 'package)
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
-
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; use-package
+;; https://github.com/jwiegley/use-package
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure t)
 
+
+;;;;;;;;;;;;;;;;
+;; packages
+
+;; https://github.com/rranelli/auto-package-update.el
 (use-package auto-package-update
   :custom
   (auto-package-update-interval 7)
@@ -41,8 +65,10 @@
   (auto-package-update-maybe)
   (auto-package-update-at-time "09:00"))
 
+;; https://github.com/emacscollective/no-littering
 (use-package no-littering)
 
+;; https://github.com/justbur/emacs-which-key
 (use-package which-key
   :defer 0
   :diminish which-key-mode
@@ -50,24 +76,86 @@
   (which-key-mode)
   (setq which-key-idle-delay 1))
 
-(use-package lsp-mode
-  :commands (lsp lsp-deferred)
-  :hook (lsp-mode . efs/lsp-mode-setup)
-  :init
-  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
-  :config
-  (lsp-enable-which-key-integration t))
-
-(use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode)
-  :custom
-  (lsp-ui-doc-position 'bottom))
-
+;; https://github.com/magit/magit
 (use-package magit
   :commands magit-status
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
 
+;; https://github.com/joaotavora/yasnippet
+(use-package yasnippet
+  :ensure t)
+
+;; https://github.com/emacs-lsp/lsp-mode
+(use-package lsp-mode
+  :ensure t
+  :init
+  (setq lsp-keymap-prefix "C-c l")  ;; Or 'C-l', 's-l'
+  :commands (lsp lsp-deferred)
+  :hook ( ;; NOTE: add `(XXX-mode . lsp-deferred)`s below
+          (go-mode . lsp-deferred)
+
+         ;; which-key integration
+         (lsp-mode . lsp-enable-which-key-integration)))
+
+;; https://github.com/emacs-lsp/lsp-ui
+(use-package lsp-ui
+  :ensure t
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-doc-position 'bottom))
+
+;; https://github.com/emacs-lsp/dap-mode
+(use-package dap-mode)
+
+;; https://github.com/company-mode/company-mode
+(use-package company
+  :ensure t
+  :config (progn
+            ;; don't add any dely before trying to complete thing being typed
+            ;; the call/response to gopls is asynchronous so this should have little
+            ;; to no affect on edit latency
+            (setq company-idle-delay 0)
+            ;; start completing after a single character instead of 3
+            (setq company-minimum-prefix-length 1)
+            ;; align fields in completions
+            (setq company-tooltip-align-annotations t)
+            ))
+
+;;
+;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;
+;; LSP/DAP packages
+
+;; (golang) https://github.com/golang/tools/blob/master/gopls/doc/emacs.md
+
+;; https://github.com/dominikh/go-mode.el
+(use-package go-mode
+  :ensure t
+  :bind (
+         ;; If you want to switch existing go-mode bindings to use lsp-mode/gopls instead
+         ;; uncomment the following lines
+         ;; ("C-c C-j" . lsp-find-definition)
+         ;; ("C-c C-d" . lsp-describe-thing-at-point)
+         )
+  :hook ((go-mode . lsp-deferred)
+         (before-save . lsp-format-buffer)
+         (before-save . lsp-organize-imports)))
+
+(lsp-register-custom-settings
+ '(("gopls.completeUnimported" t t)
+   ("gopls.staticcheck" t t)))
+
+;; https://emacs-lsp.github.io/dap-mode/page/configuration/#go
+(require 'dap-dlv-go)
+
+;;
+;;;;;;;;;;;;;;;;
+
+
+;;
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
