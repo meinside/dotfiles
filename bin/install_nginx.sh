21b9268e397cb48da3fd22cbdc9ebdd4fb819ba2
Sungjin Han <meinside@meinside.dev>
2024-08-19 16:04:19 +0900

    Edit bin/install_nginx.sh

--- bin/install_nginx.sh
+++ bin/install_nginx.sh
@@ -41,7 +41,7 @@
 #   0 5 1 */1 * sudo tailscale cert --cert-file /path/to/cert.crt --key-file /path/to/cert.key "subdomain.my-tailnet-name.ts.net"
 #
 # created on : 2017.08.16.
-# last update: 2024.06.17.
+# last update: 2024.08.19.
 
 
 ################################
@@ -49,7 +49,7 @@
 # frequently updated values
 
 # nginx/library versions
-NGINX_VERSION="1.26.1"  # https://nginx.org/en/download.html
+NGINX_VERSION="1.26.2"  # https://nginx.org/en/download.html
 OPENSSL_VERSION="3.3.1" # https://github.com/openssl/openssl/tags
 ZLIB_VERSION="1.3.1"	# https://github.com/madler/zlib/tags
 PCRE_VERSION="10.44"	# https://github.com/PCRE2Project/pcre2/releases
