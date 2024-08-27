5bb41cd8cb97f13344e903ae6c8e1f899a3fce52
Sungjin Han <meinside@meinside.dev>
2024-08-27 16:02:07 +0900

    Edit bin/install_ffmpeg.sh

--- bin/install_ffmpeg.sh
+++ bin/install_ffmpeg.sh
@@ -6,7 +6,7 @@
 #
 # (pass '--do-not-clean' argument for preserving files after install)
 # 
-# last update: 2024.05.30.
+# last update: 2024.08.27.
 
 
 ################################
@@ -14,7 +14,7 @@
 # frequently updated values
 
 # https://github.com/FFmpeg/FFmpeg/tags
-FFMPEG_VERSION="n7.0.1" # XXX - edit for newer ffmpeg version
+FFMPEG_VERSION="n7.0.2" # XXX - edit for newer ffmpeg version
 
 
 ################################
