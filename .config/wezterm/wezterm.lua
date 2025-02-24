11eac4d077fa3d4ef1096a05bf9b64f0335e9f50
Sungjin Han <sun.han@tosslab.com>
2025-02-24 17:05:14 +0900

    Edit .config/tmux/ and .config/wezterm things

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,32 +1,32 @@
 -- $XDG_CONFIG_HOME/wezterm/wezterm.lua
 --
--- last update: 2024.10.28.
+-- last update: 2025.02.24.
 
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 --------------------------------
 -- my configurations
-config.color_scheme = 'Peppermint (Gogh)'
+config.color_scheme = "Peppermint (Gogh)"
 config.font = wezterm.font_with_fallback({
-  'JetBrainsMono Nerd Font Mono',
-  'Iosevka Rootiest v2',
-  'Flog Symbols',
-  'Symbols Nerd Font',
-  'Material Icons',
-  'Noto Color Emoji',
-  'JetBrains Mono',
-  'Fira Code',
+	"JetBrainsMono Nerd Font Mono",
+	"Iosevka Rootiest v2",
+	"Flog Symbols",
+	"Symbols Nerd Font",
+	"Material Icons",
+	"Noto Color Emoji",
+	"JetBrains Mono",
+	"Fira Code",
 })
 config.initial_cols = 160
 config.initial_rows = 50
-config.window_padding = { left = '0.5cell', right = '0.5cell', top = 0, bottom = 0 }
+config.window_padding = { left = "0.5cell", right = "0.5cell", top = 0, bottom = 0 }
+config.use_cap_height_to_scale_fallback_fonts = true
 --
 --------------------------------
 
 return config
-
