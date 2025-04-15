ba6b7e7862153a24a35808f23954b0390158aedf
Sungjin Han <meinside@duck.com>
2025-04-15 14:24:17 +0900

    Edit .config/nvim/lua/plugins/lsps.lua

--- .config/nvim/lua/plugins/lsps.lua
+++ .config/nvim/lua/plugins/lsps.lua
@@ -1,74 +1,51 @@
 -- .config/nvim/lua/plugins/lsps.lua
 --
--- last update: 2025.03.27.
+-- last update: 2025.04.15.
 
 local custom = require("custom") -- ~/.config/nvim/lua/custom/init.lua
 
 return {
 	{
 		"ray-x/lsp_signature.nvim",
-		event = "VeryLazy",
+		event = "InsertEnter",
 		opts = {
 			bind = true,
 			handler_opts = { border = "single" },
-			hi_parameter = "CurSearch",
 		},
-		config = function(_, opts)
-			require("lsp_signature").setup(opts)
-		end,
 	},
 	{
 		"onsails/lspkind-nvim",
 		config = function()
-			-- (gray)
-			vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", {
-				bg = "NONE",
-				fg = "#808080",
-				strikethrough = true,
-			})
-			-- (blue)
-			vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", {
-				bg = "NONE",
-				fg = "#569CD6",
-			})
-			vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", {
-				bg = "NONE",
-				fg = "#569CD6",
-			})
-			-- (light blue)
-			vim.api.nvim_set_hl(0, "CmpItemKindVariable", {
-				bg = "NONE",
-				fg = "#9CDCFE",
-			})
-			vim.api.nvim_set_hl(0, "CmpItemKindInterface", {
-				bg = "NONE",
-				fg = "#9CDCFE",
-			})
-			vim.api.nvim_set_hl(0, "CmpItemKindText", {
-				bg = "NONE",
-				fg = "#9CDCFE",
-			})
-			-- (pink)
-			vim.api.nvim_set_hl(0, "CmpItemKindFunction", {
-				bg = "NONE",
-				fg = "#C586C0",
-			})
-			vim.api.nvim_set_hl(0, "CmpItemKindMethod", {
-				bg = "NONE",
-				fg = "#C586C0",
-			})
-			-- (front)
-			vim.api.nvim_set_hl(0, "CmpItemKindKeyword", {
-				bg = "NONE",
-				fg = "#D4D4D4",
-			})
-			vim.api.nvim_set_hl(0, "CmpItemKindProperty", {
-				bg = "NONE",
-				fg = "#D4D4D4",
-			})
-			vim.api.nvim_set_hl(0, "CmpItemKindUnit", {
-				bg = "NONE",
-				fg = "#D4D4D4",
+			require("lspkind").init({
+				mode = "symbol_text",
+				preset = "default",
+				symbol_map = {
+					Text = "󰉿",
+					Method = "󰆧",
+					Function = "󰊕",
+					Constructor = "",
+					Field = "󰜢",
+					Variable = "󰀫",
+					Class = "󰠱",
+					Interface = "",
+					Module = "",
+					Property = "󰜢",
+					Unit = "󰑭",
+					Value = "󰎠",
+					Enum = "",
+					Keyword = "󰌋",
+					Snippet = "",
+					Color = "󰏘",
+					File = "󰈙",
+					Reference = "󰈇",
+					Folder = "󰉋",
+					EnumMember = "",
+					Constant = "󰏿",
+					Struct = "󰙅",
+					Event = "",
+					Operator = "󰆕",
+					TypeParameter = "",
+				},
 			})
 		end,
 	},
