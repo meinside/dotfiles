1f37499ca2d84fda7572f6dd9288a36cfc8b3950
Sungjin Han <meinside@duck.com>
2025-04-22 13:09:52 +0900

    Edit .config/nvim things

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,27 +2,36 @@
 --
 -- https://www.lazyvim.org/keymaps
 --
--- last update: 2025.04.02.
+-- last update: 2025.04.22.
 
--- DO NOT USE `LazyVim.safe_keymap_set` IN YOUR OWN CONFIG!!
--- use `vim.keymap.set` instead
-local map = LazyVim.safe_keymap_set
+-- copy, paste below, and comment the current line
+vim.keymap.set("n", "ycc", "yygccp", {
+	remap = true,
+	desc = "Copy, paste below, and comment the current line",
+})
+
+-- search within visual selection
+vim.keymap.set("x", "/", "<Esc>/\\%V")
 
 -- tab navigations
-map("n", "<C-h>", ":tabprevious<CR>", {
+--
+-- \bb for switching buffers,
+-- \bd for closing a buffer,
+--
+vim.keymap.set("n", "<C-h>", ":tabprevious<CR>", {
 	desc = "Previous tab",
 }) -- <ctrl-h> for previous tab
-map("n", "<C-l>", ":tabnext<CR>", {
+vim.keymap.set("n", "<C-l>", ":tabnext<CR>", {
 	desc = "Next tab",
 }) -- <ctrl-l> for next tab
 
 -- for toggling mouse: `\mm`
-map("n", "<leader>mm", function()
+vim.keymap.set("n", "<leader>mm", function()
 	require("tools").ui.toggle_mouse() -- ~/.config/nvim/lua/tools.lua
 end, { desc = "mouse: Toggle" })
 
 -- for toggling diagnostics: `\tD`
-map("n", "<leader>tD", function()
+vim.keymap.set("n", "<leader>tD", function()
 	if vim.diagnostic.is_enabled() then
 		vim.diagnostic.enable(false)
 	else
@@ -32,13 +41,13 @@ map("n", "<leader>tD", function()
 end, { desc = "diagnostics: Toggle" })
 
 -- <Left> for folding, <Right> for unfolding
-map("n", "<Left>", require("origami").h)
-map("n", "<Right>", require("origami").l)
+vim.keymap.set("n", "<Left>", require("origami").h)
+vim.keymap.set("n", "<Right>", require("origami").l)
 
 -- (minifiles)
 --
 -- for toggling minifiles: `\mf`
-map("n", "<leader>mf", function()
+vim.keymap.set("n", "<leader>mf", function()
 	require("mini.files").setup({})
 	MiniFiles.open()
 end, { desc = "mini-files: Open" })
@@ -47,23 +56,23 @@ end, { desc = "mini-files: Open" })
 --
 -- https://github.com/nvim-telescope/telescope.nvim#pickers
 local builtin = require("telescope.builtin")
-map("n", "<leader>tt", builtin.builtin, {
+vim.keymap.set("n", "<leader>tt", builtin.builtin, {
 	desc = "telescope: List builtin pickers",
 })
-map("n", "<leader>ff", builtin.find_files, {
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {
 	desc = "telescope: Find files",
 })
-map("n", "<leader>gc", builtin.git_commits, {
+vim.keymap.set("n", "<leader>gc", builtin.git_commits, {
 	desc = "telescope: Git commits",
 })
-map("n", "<leader>qf", builtin.quickfix, {
+vim.keymap.set("n", "<leader>qf", builtin.quickfix, {
 	desc = "telescope: Quickfix",
 })
 
 -- (lsp)
 --
 -- for toggling inlay hint: `\li`
-map("n", "<leader>li", function()
+vim.keymap.set("n", "<leader>li", function()
 	local enabled = not vim.lsp.inlay_hint.is_enabled({})
 	vim.lsp.inlay_hint.enable(enabled)
 
@@ -73,7 +82,7 @@ end, { desc = "lsp: Toggle inlay hint" })
 -- (minimap)
 --
 -- for toggling minimap: `\tm`
-map("n", "<leader>tm", function()
+vim.keymap.set("n", "<leader>tm", function()
 	require("codewindow").toggle_minimap()
 
 	vim.notify("Toggled minimap.")
@@ -82,18 +91,18 @@ end, { desc = "minimap: Toggle" })
 -- (code actions)
 --
 -- `\ca` for showing code action previews
-map({ "v", "n" }, "ca", require("actions-preview").code_actions, { desc = "actions-preview: Code actions" })
+vim.keymap.set({ "v", "n" }, "ca", require("actions-preview").code_actions, { desc = "actions-preview: Code actions" })
 
 -- (codeium)
 --
 -- alt-e: cycle through suggestions
-map("i", "<A-e>", function()
+vim.keymap.set("i", "<A-e>", function()
 	if require("custom").features().codeium then
 		require("neocodeium").cycle_or_complete()
 	end
 end, { desc = "Neocodeium: Cycle through suggestions" })
 -- alt-f: accept
-map("i", "<A-f>", function()
+vim.keymap.set("i", "<A-f>", function()
 	if require("custom").features().codeium then
 		require("neocodeium").accept()
 	end
