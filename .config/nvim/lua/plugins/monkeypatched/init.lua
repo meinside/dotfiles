5cf91f72016b459c6dd1c2577ac05b0822eeffc8
Sungjin Han <sun.han@tosslab.com>
2025-08-22 17:12:17 +0900

    Edit .config/nvim/lua files

---
+++ .config/nvim/lua/plugins/monkeypatched/init.lua
@@ -0,0 +1,13 @@
+-- NOTE: put monkey-patched lua plugins here
+--
+-- For monkey-patching lua plugins, put lua files in this directory with content like:
+--
+-- ```lua
+-- return {
+--    "some/lua-plugin/that-needs-monkey-patch.nvim",
+--    init = function()
+-- do some monkey-patching here
+--    end,
+-- }
+-- ```
+return {}
