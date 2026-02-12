e7b5ec54de012fe4bd56341499b253e1c03cb77d
Sungjin Han <meinside@duck.com>
2026-02-12 17:46:36 +0900

    Edit bin/install_nginx.sh

--- bin/install_nginx.sh
+++ bin/install_nginx.sh
@@ -46,16 +46,16 @@
 #   #ExecStartPre=/bin/mkdir -p /var/log/nginx
 #
 # created on : 2017.08.16.
-# last update: 2026.01.05.
+# last update: 2026.02.12.
 
 ################################
 #
 # frequently updated values
 
 # nginx/library versions
-NGINX_VERSION="1.29.4"  # https://nginx.org/en/download.html
-OPENSSL_VERSION="3.6.0" # https://github.com/openssl/openssl/tags
-ZLIB_VERSION="1.3.1"    # https://github.com/madler/zlib/tags
+NGINX_VERSION="1.29.5"  # https://nginx.org/en/download.html
+OPENSSL_VERSION="3.6.1" # https://github.com/openssl/openssl/tags
+ZLIB_VERSION="1.3.1.2"  # https://github.com/madler/zlib/tags
 PCRE_VERSION="10.47"    # https://github.com/PCRE2Project/pcre2/releases
 
 ################################
