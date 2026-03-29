96933f8b5ad634a19fc69a688c6d7be0c4dbb73f
Sungjin Han <sun.han@tosslab.com>
2026-03-30 08:41:46 +0900

    Edit bin/install_neovim.sh

--- bin/install_neovim.sh
+++ bin/install_neovim.sh
@@ -5,7 +5,7 @@
 # For building neovim from source code.
 # (https://github.com/neovim/neovim/wiki/Installing-Neovim#install-from-source)
 #
-# last update: 2026.01.27.
+# last update: 2026.03.30.
 
 # * To install nightly version:
 #
@@ -38,7 +38,7 @@
 # frequently updated values
 
 # https://github.com/neovim/neovim/releases
-NVIM_VERSION="v0.11.6" # XXX - edit for other versions
+NVIM_VERSION="v0.12.0" # XXX - edit for other versions
 
 ################################
 #
