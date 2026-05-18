147ee42365008c5732bf88f66cefdc510a62e3c8
Sungjin Han <meinside@duck.com>
2026-05-18 17:28:47 +0900

    Edit bin/update_motd.sh

--- bin/update_motd.sh
+++ bin/update_motd.sh
@@ -3,9 +3,9 @@
 # bin/update_motd.rb
 #
 # (used: https://manytools.org/hacker-tools/convert-images-to-ascii-art/)
-# 
+#
 # created on : 2012.08.21.
-# last update: 2023.07.05.
+# last update: 2026.05.18.
 
 read -r -d '' ASCII_ART <<EOF
 .                            ...........
@@ -43,7 +43,7 @@ EOF
 if [ -f /etc/motd ]; then
 	sudo bash -c "cat << EOF > /etc/motd
 $ASCII_ART
-EOF" && \
+EOF" &&
 		echo "updated: /etc/motd"
 elif [ -d /etc/update-motd.d/ ]; then
 	sudo bash -c "cat << EOF > /etc/update-motd.d/09-ascii-art
@@ -54,10 +54,9 @@ elif [ -d /etc/update-motd.d/ ]; then
 cat << ASCII_ART
 $ASCII_ART
 ASCII_ART
-EOF" && \
-		sudo chown root.root /etc/update-motd.d/09-ascii-art && \
-		sudo chmod +x /etc/update-motd.d/09-ascii-art && \
+EOF" &&
+		sudo chown root:root /etc/update-motd.d/09-ascii-art &&
+		sudo chmod +x /etc/update-motd.d/09-ascii-art &&
 		echo "updated: /etc/update-motd.d/09-ascii-art"
 
 fi
-
